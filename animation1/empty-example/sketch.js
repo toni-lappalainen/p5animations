552fa15eb3bd6df2bcfdfe0354ff58310e08b1fd
Toni <toni.lappalainen@gmail.com>
2020-07-29 20:23:42 +0300

    basics done

---
+++ animation1/empty-example/sketch.js
@@ -0,0 +1,76 @@
+let snowflakes = []; // array to hold snowflake objects
+
+let song;
+let fft;
+
+function setup() {
+  createCanvas(800, 600);
+  fill('white');
+  noStroke();
+}
+
+
+function preload() {
+  song = loadSound('');
+}
+
+var col = {
+  r: 255,
+  g: 0,
+  b: 0
+};
+
+function draw() {
+  background('black');
+  let t = frameCount / 60; // update time
+  
+  col.r = random(0, 255)
+  col.g = 0
+  col.b = random(0,255)
+  
+  fill(col.r, col.g, col.b, 255);
+
+  // create a random number of snowflakes each frame
+  for (let i = 0; i < random(5); i++) {
+    snowflakes.push(new snowflake()); // append snowflake object
+  }
+
+  // loop through snowflakes with a for..of loop
+  for (let flake of snowflakes) {
+    flake.update(t); // update snowflake position
+    flake.display(); // draw snowflake
+  }
+}
+
+// snowflake class
+function snowflake() {
+  // initialize coordinates
+  this.posX = 0;
+  this.posY = random(-50, 0);
+  this.initialangle = random(0, 2 * PI);
+  this.size = random(2, 5);
+
+  // radius of snowflake spiral
+  // chosen so the snowflakes are uniformly spread out in area
+  this.radius = sqrt(random(pow(width / 2, 2)));
+
+  this.update = function(time) {
+    // x position follows a circle
+    let w = 0.6; // angular speed
+    let angle = w * time + this.initialangle;
+    this.posX = width / 2 + this.radius * sin(angle);
+
+    // different size snowflakes fall at slightly different y speeds
+    this.posY += pow(this.size, 0.5);
+
+    // delete snowflake if past end of screen
+    if (this.posY > height) {
+      let index = snowflakes.indexOf(this);
+      snowflakes.splice(index, 1);
+    }
+  };
+
+  this.display = function() {
+    ellipse(this.posX, this.posY, this.size);
+  };
+}
